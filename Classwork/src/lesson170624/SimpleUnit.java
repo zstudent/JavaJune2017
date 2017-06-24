a2dbbfd9bce872c27ddd02b6a328ad9588a519d7
Zaal Lyanov <Zaal_Lyanov@epam.com>
2017-06-24 13:26:40 +0300

    added assertEquals/notEquals for int int

--- Classwork/src/lesson170624/SimpleUnit.java
+++ Classwork/src/lesson170624/SimpleUnit.java
@@ -18,4 +18,20 @@ public class SimpleUnit {
 		return notEquals;
 	}
 
+	public static boolean assertEquals(int actual, int expected) {
+		boolean equals = actual == expected;
+		if (!equals) {
+			System.err.println("Test failed: expected " + expected + ", but actual " + actual);
+		}
+		return equals;
+	}
+
+	public static boolean assertNotEquals(int actual, int expected) {
+		boolean notEquals = actual != expected;
+		if (!notEquals) {
+			System.err.println("Test failed: expected NOT " + expected + ", but actual " + actual);
+		}
+		return notEquals;
+	}
+	
 }
